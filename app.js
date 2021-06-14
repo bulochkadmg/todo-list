00875e588526bf0edeee3b2b5565ab62f6afb122
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-06-14 23:09:00 +0300

    save themes to localstorage

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ const tasks = [
       '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
     },
   };
-  let lastSelectedTheme = 'default'; 
+  let lastSelectedTheme = localStorage.getItem('app_theme') || 'default'; 
 
   // Elemnts UI
   const listContainer = document.querySelector(
@@ -117,6 +117,7 @@ const tasks = [
   const themeSelect = document.getElementById('themeSelect');
 
   // Events
+  setTheme(lastSelectedTheme);
   renderAllTasks(objOfTasks);
   form.addEventListener('submit', onFormSubmitHandler);
   listContainer.addEventListener('click', onDeletehandler);
@@ -227,6 +228,7 @@ const tasks = [
     };
     setTheme(selectedTheme);
     lastSelectedTheme = selectedTheme;
+    localStorage.setItem('app_theme', selectedTheme)
   }
 
   function setTheme(name) {
